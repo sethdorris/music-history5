2be4d061eb169c380fdc0e57f1f15237a4e467e8
Seth Dorris <seth@Seths-MacBook-Pro.local>
2015-08-07 13:59:58 -0500

    added local firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,22 +3,30 @@ requirejs.config({
   paths: {
     'jquery': '../bower_components/jquery/dist/jquery.min',
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'firebase': '../bower_components/firebase/firebase'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
 
 
 requirejs(
-  ["jquery", "hbs", "bootstrap", "dom-access", "populate-songs", "get-more-songs"], 
-  function($, Handlebars, bootstrap, dom, songs1, songs2) {
+  ["jquery", "firebase", "hbs", "bootstrap", "dom-access", "populate-songs", "get-more-songs"], 
+  function($, _firebase, Handlebars, bootstrap, dom, songs1, songs2) {
     
     var $dom = dom.getOutputElement();
     var moreSongsArray = [];
     var $panel = $("#myPanel");
+    var myFirebase = new Firebase("https://seth-music-history.firebaseio.com");
+    myFirebase.child("songs").on("value", function(snapshot) {
+      console.log(snapshot.val());  
+    });
 
 
 // Populate-Songs Callback Function
@@ -27,12 +35,7 @@ requirejs(
     require(['hbs!../templates/songs', 'hbs!../templates/panel'], function(domTemplate, panelTemplate) {
     	$dom.html(domTemplate(songs1));
     	$panel.html(panelTemplate(songs1));
-    });
-
-  //       for (var i = 0; i < songs1.songs.length; i++) {
-  //   	$dom.append("<div class='content-song'>" + songs1.songs[i] + "</div>" + "<div class='content-artist'>" + songs1.artist[i] + "<span class='content-album'>" + "<em>off the album: </em>" + songs1.album[i] + "</span>" + "<button class='delete'>X</button>" + "</div>");
-		// }
-	 //    $dom.append("<button id='more'>More>></button>");
+      });
     };
 
 
@@ -40,21 +43,7 @@ requirejs(
     songs1.runAjax(songs1Data);
   
 	$(document).on("click", ".delete", function() {
-		  var $deleteClicked = $(this);
-		  var $deleteParent = $deleteClicked.parent();
-		  var $parentPrevious = $deleteParent.prev();
-		  console.log($deleteParent);
-		  $deleteParent.hide();
-		  $parentPrevious.hide();
-
-      // $.ajax({
-      //   url: 'https://seth-music-history.firebaseio.com/songs.json',
-      //   method: "DELETE",
-      //   data: JSON.stringify(newAdd)
-      // }).done(function(addedsong) {
-      //   console.log(addedsong);
-      // });
-
+		  console.log(songs1);
 		});
 
   $(document).on("click", "#filter-button", function() {
@@ -83,7 +72,6 @@ requirejs(
         require(['hbs!../templates/songs', 'hbs!../templates/panel'], function (domTemplate, panelTemplate) {
           console.log("filtered array: ", filteredArray); 
           var finalHTML = domTemplate({songs: filteredArray});
-          console.log("finalHTML", finalHTML);
           $dom.html(finalHTML);
           $panel.html(panelTemplate({songs: filteredArray}));
         })
